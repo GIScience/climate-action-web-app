9e0d345dcaf3a3658de1f3110320b66958fd440b
Maciej Adamiak <maciej.adamiak@heigit.org>
2023-12-04 08:36:17 +0000

    feat: standalone page not found component

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core'
+import {NgOptimizedImage} from '@angular/common'
 
 @Component({
     selector: 'app-page-not-found',
     templateUrl: './page-not-found.component.html',
-    styleUrls: ['./page-not-found.component.scss']
+    styleUrls: ['./page-not-found.component.scss'],
+    imports: [
+        NgOptimizedImage
+    ],
+    standalone: true
 })
 export class PageNotFoundComponent {
 
