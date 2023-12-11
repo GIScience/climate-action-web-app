ed98b0d8532baadf6cac4ae78a79e2741379025a
Maciej Adamiak <maciej.adamiak@heigit.org>
2023-12-11 12:07:45 +0000

    refactor: slider

--- src/app/plugins/plugins.component.ts
+++ src/app/plugins/plugins.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core'
+import {AfterViewInit, Component} from '@angular/core'
 import {Router} from '@angular/router'
 import {PluginService} from '../plugin/plugin.service'
 import {CommonModule} from '@angular/common'
@@ -19,76 +19,18 @@ import {availableCards, PluginCard} from './plugins.interface';
     ],
     standalone: true
 })
-export class PluginsComponent implements OnInit, AfterViewInit {
+export class PluginsComponent implements AfterViewInit {
 
     cards: Array<PluginCard> = availableCards
+    activeCard?: PluginCard
 
     constructor(
         private router: Router,
         private pluginService: PluginService) {
     }
 
-    ngOnInit(): void {
-        setTimeout(() => {
-            this.calculateProgressBar()
-        }, 200)
-    }
-
     ngAfterViewInit(): void {
-        const slider = document.querySelector('.slider') as HTMLElement
-        slider.style.setProperty('--slider-index', '0')
         this.loadPlugins()
-    }
-
-    calculateProgressBar() {
-        const progressBarElements = document.querySelectorAll('.progress-bar')
-        progressBarElements.forEach((progressBar) => {
-            progressBar.innerHTML = ''
-
-            const slider = document.querySelector('.slider') as HTMLElement
-            if (slider == null) return
-            const itemCount = slider.children.length
-            const itemsPerScreen = parseInt(getComputedStyle(slider).getPropertyValue('--items-per-screen'))
-            let sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'))
-            const progressBarItemCount = Math.ceil(itemCount / itemsPerScreen);
-
-            if (sliderIndex >= progressBarItemCount) {
-                slider.style.setProperty('--slider-index', progressBarItemCount - 1 + '')
-                sliderIndex = progressBarItemCount - 1
-            }
-
-            for (let i = 0; i < progressBarItemCount; i++) {
-                const barItem = document.createElement('div')
-                barItem.classList.add('progress-item')
-                if (i === sliderIndex) {
-                    barItem.classList.add('active')
-                }
-                progressBar.appendChild(barItem)
-            }
-        })
-    }
-
-    onSlideClick(direction: string) {
-        const progressBar = document.querySelector('.progress-bar')
-        const slider = document.querySelector('.slider') as HTMLElement
-        if (slider === null || progressBar === null) return
-
-        const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'))
-        const progressBarItemCount = progressBar.children.length
-
-        let newIndex
-
-        if (direction === 'left') {
-            newIndex = sliderIndex - 1 < 0 ? progressBarItemCount - 1 : sliderIndex - 1
-        } else if (direction === 'right') {
-            newIndex = sliderIndex + 1 >= progressBarItemCount ? 0 : sliderIndex + 1
-        } else {
-            return
-        }
-
-        slider.style.setProperty('--slider-index', newIndex + '')
-        progressBar.children[sliderIndex].classList.remove('active')
-        progressBar.children[newIndex].classList.add('active')
     }
 
     loadPlugins() {
@@ -101,6 +43,9 @@ export class PluginsComponent implements OnInit, AfterViewInit {
                         card.library_version = plugin.library_version
                         card.version = plugin.version
                         card.purpose = plugin.purpose
+
+                        if (this.router.url == `/plugin/${card.plugin_id}`)
+                            this.activeCard = card
                     }
                 })
             },
@@ -110,7 +55,13 @@ export class PluginsComponent implements OnInit, AfterViewInit {
         })
     }
 
-    showPluginInfo(pluginId: string) {
-        this.router.navigate(['plugin', pluginId])
+    activateCard(card: PluginCard) {
+        if (card.enabled)
+            this.activeCard = card
+    }
+
+    showPluginInfo(card: PluginCard) {
+        if (card.enabled)
+            this.router.navigate(['plugin', card.plugin_id])
     }
 }
