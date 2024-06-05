ebb7e2fda912cb980ef238af3a88a15fdcfa17a6
Ashwin Chandran <ashwin.chandran@heigit.org>
2024-06-05 18:39:10 +0000

    fix baseUrl for footnote hyperlinks in markdown

--- src/app/report/markdown/markdown.component.spec.ts
+++ src/app/report/markdown/markdown.component.spec.ts
@@ -30,13 +30,12 @@ describe('MarkdownComponent', () => {
         fixture.detectChanges()
     })
 
-    it('should create', () => {
-        expect(component).toBeTruthy()
-    })
-
     it('should render markdown', () => {
+        const testBaseUrl = location.href.split('#')[0]
+
         const markdown = '## Test Title\nThis is a test paragraph\n## Test Table\n| Table Title 1| Table Title 2 |\n|:-----|-----:|\n| Table Cell 1|Table Cell 2 |\n\n## Test Unordered List\n- List item 1\n- List item 2\n\n## Test Ordered List\n1. List Item 1\n2. List Item 2\n\n## Test Code Block\n```json\n{\n    "attribute 1": true,\n    "array 1": [\n        "Option 1"\n    ]\n}\n```\n\n## Test Footnote\nFootnote reference [^1]\n[^1]: Footnote item'
-        const html = '<h2>Test Title</h2>\n<p>This is a test paragraph</p>\n<h2>Test Table</h2>\n<table>\n<thead>\n<tr><th style="text-align:left">Table Title 1</th><th style="text-align:right">Table Title 2</th></tr>\n</thead>\n<tbody>\n<tr><td style="text-align:left">Table Cell 1</td><td style="text-align:right">Table Cell 2</td></tr>\n</tbody>\n</table>\n<h2>Test Unordered List</h2>\n<ul>\n<li>List item 1</li>\n<li>List item 2</li>\n</ul>\n<h2>Test Ordered List</h2>\n<ol>\n<li>List Item 1</li>\n<li>List Item 2</li>\n</ol>\n<h2>Test Code Block</h2>\n<pre><code class="language-json">{\n    "attribute 1": true,\n    "array 1": [\n        "Option 1"\n    ]\n}\n</code></pre>\n<h2>Test Footnote</h2>\n<p>Footnote reference <sup class="footnote-ref"><a href="/dashboard#fn1" id="fnref1">[1]</a></sup></p>\n<hr class="footnotes-sep">\n<section class="footnotes">\n<ol class="footnotes-list">\n<li id="fn1" class="footnote-item"><p>Footnote item <a href="/dashboard#fnref1" class="footnote-backref">↩</a></p>\n</li>\n</ol>\n</section>'
+
+        const html = '<h2>Test Title</h2>\n<p>This is a test paragraph</p>\n<h2>Test Table</h2>\n<table>\n<thead>\n<tr><th style="text-align:left">Table Title 1</th><th style="text-align:right">Table Title 2</th></tr>\n</thead>\n<tbody>\n<tr><td style="text-align:left">Table Cell 1</td><td style="text-align:right">Table Cell 2</td></tr>\n</tbody>\n</table>\n<h2>Test Unordered List</h2>\n<ul>\n<li>List item 1</li>\n<li>List item 2</li>\n</ul>\n<h2>Test Ordered List</h2>\n<ol>\n<li>List Item 1</li>\n<li>List Item 2</li>\n</ol>\n<h2>Test Code Block</h2>\n<pre><code class="language-json">{\n    "attribute 1": true,\n    "array 1": [\n        "Option 1"\n    ]\n}\n</code></pre>\n<h2>Test Footnote</h2>\n<p>Footnote reference <sup class="footnote-ref"><a href="' + testBaseUrl + '#fn1" id="fnref1">[1]</a></sup></p>\n<hr class="footnotes-sep">\n<section class="footnotes">\n<ol class="footnotes-list">\n<li id="fn1" class="footnote-item"><p>Footnote item <a href="' + testBaseUrl + '#fnref1" class="footnote-backref">↩</a></p>\n</li>\n</ol>\n</section>'
 
         httpClientSpy.get.and.returnValue(of(markdown))
 
