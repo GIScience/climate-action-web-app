f4d74ae6af1133118232000a26842188465e6af2
Ashwin Chandran <ashwin.chandran@heigit.org>
2024-06-18 11:51:05 +0000

    fix: correct relative path for images in assets

--- src/app/plugins/plugins.interface.ts
+++ src/app/plugins/plugins.interface.ts
@@ -15,43 +15,43 @@ export const availableCards = [
         enabled: false,
         plugin_id: 'plugin_blueprint',
         name: 'Plugin Blueprint',
-        image: '../../assets/images/plugin-covers/blueprint.jpg',
+        image: 'assets/images/plugin-covers/blueprint.jpg',
         image_source: 'https://unsplash.com/@sxoxm'
     }, {
         enabled: false,
         plugin_id: 'lulc_change_emission_estimation',
         name: 'GHG Emission from LULC Change',
-        image: '../../assets/images/plugin-covers/ghg-lulc.jpg',
+        image: 'assets/images/plugin-covers/ghg-lulc.jpg',
         image_source: 'https://unsplash.com/@bazylu'
     }, {
         enabled: false,
         plugin_id: 'greenness',
         name: 'Greenness',
-        image: '../../assets/images/plugin-covers/greenness.jpg',
+        image: 'assets/images/plugin-covers/greenness.jpg',
         image_source: 'https://unsplash.com/@chuttersnap'
     }, {
         enabled: false,
         plugin_id: 'bikeability',
         name: 'Bikeability',
-        image: '../../assets/images/plugin-covers/bikeability.jpg',
+        image: 'assets/images/plugin-covers/bikeability.jpg',
         image_source: 'https://unsplash.com/@waldemarbrandt67w'
     }, {
         enabled: false,
         plugin_id: 'drinking_water',
         name: 'Drinking Water',
-        image: '../../assets/images/plugin-covers/drinking-water.jpg',
+        image: 'assets/images/plugin-covers/drinking-water.jpg',
         image_source: 'https://unsplash.com/@dantakesphotos'
     }, {
         enabled: false,
         plugin_id: 'soil_consumption',
         name: 'Soil Consumption',
-        image: '../../assets/images/plugin-covers/soil-consumption.jpg',
+        image: 'assets/images/plugin-covers/soil-consumption.jpg',
         image_source: 'https://unsplash.com/@synkevych'
     },{
         enabled: false,
         plugin_id: 'walkability',
         name: 'Walkability',
-        image: '../../assets/images/plugin-covers/walkability.jpg',
+        image: 'assets/images/plugin-covers/walkability.jpg',
         image_source: 'https://unsplash.com/@paulmelki'
     }
 ] as PluginCard[]
