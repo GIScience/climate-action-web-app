c927e150bde4a75e7b9a45e19b3507e9dbb58312
Ashwin Chandran <ashwin.chandran@heigit.org>
2024-06-19 14:37:52 +0000

    fix: failing pipeline by including rules under cypress test and updating container
    and much more
    
    Co-authored-by: Moritz Schott <moritz.schott@heigit.org>

--- cypress/e2e/externalRequests.cy.js
+++ cypress/e2e/externalRequests.cy.js
@@ -5,15 +5,16 @@ describe('External Requests', () => {
     ];
 
     const cypressInternalPatterns = [
-        'https://content-autofill.googleapis.com',
-        'https://optimizationguide-pa.googleapis.com'
+        'https://firefox.settings.services.mozilla.com',
+        'https://tracking-protection.cdn.mozilla.net',
+        'https://firefox-settings-attachments.cdn.mozilla.net'
     ];
 
     it('should only make external calls to allowlisted URLs', () => {
         cy.intercept('*', (req) => {
             const isAllowlisted = allowlist.some(url => req.url.startsWith(url));
             const isCypressInternal = cypressInternalPatterns.some(url => req.url.startsWith(url));
-            
+
             if (!isAllowlisted && !isCypressInternal) {
                 throw new Error(`The request URL ${req.url} is not allowlisted`);
             }
