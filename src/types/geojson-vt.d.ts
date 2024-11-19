a2293ee1f2f1c3806740bf1ae06987c0cc33cc52
Ashwin Chandran <ashwin.chandran@heigit.org>
2024-11-19 09:34:35 +0000

    fix(maps): optimise rendering of geojson by slicing it into vector tiles feat(test): added visual regression testing under Cypress

---
+++ src/types/geojson-vt.d.ts
@@ -0,0 +1,6 @@
+declare module 'geojson-vt' {
+    // Allow for 'any' since GeoJSONVTFeature's type defs clash with those of OL
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const geojsonvt: any
+    export default geojsonvt
+} 
