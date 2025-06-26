c800a4f6f13c1eb5bb8d5e1d457148181e8467f1
Ashwin Chandran <ashwin.chandran@heigit.org>
2025-06-27 00:35:48 +0200

    ux(optional params): improve form save/reset/close attributes

--- src/app/types/dialog/dialog-window.component.ts
+++ src/app/types/dialog/dialog-window.component.ts
@@ -5,6 +5,7 @@ import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/materia
 import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core'
 import { CircleX, LucideAngularModule } from 'lucide-angular'
 import { NgScrollbarModule } from 'ngx-scrollbar'
+import { ToastrService } from 'ngx-toastr'
 
 @Component({
     selector: 'app-dialog-window',
@@ -15,12 +16,42 @@ import { NgScrollbarModule } from 'ngx-scrollbar'
 export class DialogWindowComponent {
     constructor(
         public dialogRef: MatDialogRef<DialogWindowComponent>,
+        private toastr: ToastrService,
         @Inject(MAT_DIALOG_DATA) public data: FormlyFieldConfig
     ) {}
 
     readonly CircleX = CircleX
 
     closeDialog(): void {
+        if (confirm('Are you sure you want to close without saving? Values will be reset to their defaults.')) {
+            this.resetDialog()
+            this.dialogRef.close()
+        }
+    }
+
+    saveDialog(): void {
         this.dialogRef.close()
+        this.toastr.success(`${this.data.props?.label} values have been saved.`, '', {
+            timeOut: 4000
+        })
+    }
+
+    resetDialog(): void {
+        this.resetFieldGroup(this.data.fieldGroup || [])
+        this.toastr.info(`${this.data.props?.label} values have been reset to their defaults.`, '', {
+            timeOut: 4000
+        })
+    }
+
+    private resetFieldGroup(fieldGroup: FormlyFieldConfig[]): void {
+        fieldGroup.forEach(field => {
+            if (field.fieldGroup) {
+                this.resetFieldGroup(field.fieldGroup)
+            } else if (field.formControl) {
+                field.formControl.reset()
+                field.formControl.markAsPristine()
+                field.formControl.markAsUntouched()
+            }
+        })
     }
 }
