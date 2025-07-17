0a8a59704b090f0943496c318a8bf1bc740d6293
Ashwin Chandran <ashwin.chandran@heigit.org>
2025-07-17 17:55:58 +0200

    chore(ci): replace kaniko with pure docker builder

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,7 +1,25 @@
 import 'cypress-real-events'
 import { addCompareSnapshotCommand } from 'cypress-visual-regression/dist/command'
+import { cypressEnvironment } from './cypress-environment'
 
 addCompareSnapshotCommand()
+
+beforeEach(() => {
+    cy.intercept('**', req => {
+        if (req.url.includes(`/${cypressEnvironment.apiBasePath}/`)) {
+            req.reply({
+                statusCode: 404,
+                body: {
+                    error: 'API call not mocked in Cypress test',
+                    url: req.url,
+                    method: req.method
+                }
+            })
+        } else {
+            req.continue()
+        }
+    }).as('blockUnmockedApiCalls')
+})
 
 Cypress.Commands.add('waitForRenderComplete', () => {
     cy.window().then(win => {
