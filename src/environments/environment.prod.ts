4bf5463bb8f6a3554f2b9b12017f81ae7be00414
Ashwin Chandran <ashwin.chandran@heigit.org>
2025-07-21 10:38:08 +0200

    fix(env): add type and use envType array value everywhere

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
+import { Environment } from '../types/environment'
 import { APPWRITE_ENDPOINT, APPWRITE_PROJECT_ID, APPWRITE_WEBSITE_URL, ORS_API_KEY } from './keys-and-ids/keys-and-ids'
 
-export const environment = {
-    production: true,
+export const environment: Environment = {
     environmentType: 'production',
     climateActionApiUrl: 'https://api.heigit.org/climate-action-platform',
     climateActionWSUrl: 'wss://climate-action.heigit.org',
