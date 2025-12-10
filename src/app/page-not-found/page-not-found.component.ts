7818e29768495e51fa6b735523cf1b9587b0714a
Ashwin Chandran <ashwin.chandran@heigit.org>
2025-12-10 13:56:25 +0100

    fix(404): add link to go home

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,10 +1,17 @@
-import { NgOptimizedImage } from '@angular/common'
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { Router } from '@angular/router'
+import { TranslocoModule } from '@jsverse/transloco'
 
 @Component({
     selector: 'app-page-not-found',
     templateUrl: './page-not-found.component.html',
     styleUrls: ['./page-not-found.component.scss'],
-    imports: [NgOptimizedImage]
+    imports: [TranslocoModule]
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent {
+    private router = inject(Router)
+
+    goToDashboard() {
+        this.router.navigate(['/dashboard'])
+    }
+}
